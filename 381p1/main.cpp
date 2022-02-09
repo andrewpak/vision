f177678838bc7ed094241e2bdadb3457118d344a
andrewpak <andrew7pak@gmail.com>
2022-02-08 21:31:30 -0500

    decorate main

--- 381p1/main.cpp
+++ 381p1/main.cpp
@@ -28,7 +28,7 @@ void threshold(std::ifstream &file, std::ofstream &outFile3, std::ofstream &outF
 	maxVal = 1;
 		
 	outFile3 << numRows << " " << numCols << " " << minVal << " " << maxVal << std::endl;
-
+	outFile4 << numRows << " " << numCols << " " << minVal << " " << maxVal << std::endl;
 	for(int i = 0;i < numRows;++i){
 		for(int j = 0;j < numCols;++j){
 			int pixelVal;
@@ -139,7 +139,7 @@ int main(int argc, char *argv[]){
 		
 	threshold(inFile, outFile3, outFile4, thrVal);
 
-	// collect garbage, close files
+	// collect some trash, close files
 
 	if(histAry != NULL){
 		delete [] histAry;
