da349c20d92d785c78f80e2541f5169f6b49ab44
andrewpak <andrew7pak@gmail.com>
2022-03-02 01:43:21 -0500

    working on mirrorFraming

--- CornerPreserveAverage/main.cpp
+++ CornerPreserveAverage/main.cpp
@@ -6,7 +6,7 @@ class imageProcessing {
 	
 	int numRows, numCols, minVal, maxVal;
 
-	int **framAry;
+	int **frameAry;
 	int **outAry;
 	int **thrAry;
 	int thrVal;
@@ -14,6 +14,10 @@ class imageProcessing {
 
 	public:
 		imageProcessing(std::ifstream &inFile);
+		void loadImage(std::ifstream &inFile);
+		void mirrorFraming();
+		void setThrVal(int);
+		int getThrVal();
 		void setNumRows(int);
 		void setNumCols(int);
 		void setMinVal(int);
@@ -26,9 +30,9 @@ class imageProcessing {
 
 		// some helper functions for debugging //
 
-		void check2dAry(int **array, int rows, int cols);
+		void check2dAry(int n);
 		void destroy1dAry(int *array);
-		void destroy2dAry(int **array);
+		void destroy2dArys();
 };
 
 imageProcessing::imageProcessing(std::ifstream &inFile){
@@ -37,10 +41,87 @@ imageProcessing::imageProcessing(std::ifstream &inFile){
 
 	std::cout << this->numRows << " " << this->numCols << " " << this->minVal << " " << this->maxVal << std::endl;
 
+	// dynamically allocate arrays //
 	
+	this->frameAry = new int*[numRows + 4];
+
+	for(int i = 0;i < numRows + 4;i++){
+		frameAry[i] = new int[numCols + 4];
+	}
+	
+	this->outAry = new int*[numRows + 4];
+
+	for(int i = 0;i < numRows + 4;i++){
+		this->outAry[i] = new int[numCols + 4];
+	}
+
+	this->thrAry = new int*[numRows + 4];
+
+	for(int i = 0;i < numRows + 4;i++){
+		this->thrAry[i] = new int[numCols + 4];
+	}
+
+	/////////////////////////////////
+	
+	this->loadImage(inFile);
+	this->check2dAry(1);
+	this->mirrorFraming();
+}
+
+void imageProcessing::setThrVal(int n){
+	this->thrVal = n;
+}
+
+int imageProcessing::getThrVal(){
+	return this->thrVal;
+}
+
+void imageProcessing::loadImage(std::ifstream &inFile){
+	for(int i = 2;i < this->numRows + 2;i++){
+		for(int j = 2;j < this->numCols + 2;j++){
+			inFile >> frameAry[i][j];
+		}
+	}		
+}
+
+void imageProcessing::mirrorFraming(){
+	
+	for(int i = 2;i < numCols + 4;i++){
+		if(i == 2){
+			frameAry[1][i - 1] = frameAry[i][i]; 
+			frameAry[0][0] = frameAry[3][3];
+			//frameAry[0][1] = frameAry[1][0];
+		}
+		frameAry[1][i] = frameAry[2][i];
+		frameAry[0][i] = frameAry[3][i];
+	}
+
+	for(int i = 2;i < numRows + 4;i++){
+		if(i == 2){
+			frameAry[i - 1][numCols + 2] = frameAry[i][numCols + 1];
+			frameAry[1][numCols + 3] = frameAry[1][numCols];
+			frameAry[0][numCols + 3] = frameAry[2][numCols - 2];
+		}
+		frameAry[i][numCols + 2] = frameAry[i][numCols + 1];
+		frameAry[i][numCols + 3] = frameAry[i][numCols];
+	}
+
+	for(int i = numCols + 2;i > 0;i--){
+		frameAry[numRows + 2][i] = frameAry[numRows + 1][i];
+		frameAry[numRows + 3][i] = frameAry[numRows][i];
+	}
+
+	for(int i = numRows + 2;i > 0;i--){
+		frameAry[i][1] = frameAry[i][2];
+		frameAry[i][0] = frameAry[i][3]; 
+		frameAry[0][1] = frameAry[1][0];
+	}
 
 }
 
+///
+
+///
 void imageProcessing::setNumRows(int n){
 	this->numRows = n;
 }
@@ -75,34 +156,109 @@ int imageProcessing::getMaxVal(){
 
 // helper functions .. //
 
-void imageProcessing::check2dAry(int **array, int rows, int cols){
-	for(int i = 0;i < rows;i++){
-		for(int j = 0;j < cols;j++){
-			if(array[i][j] < 10){
-				std::cout << array[i][j] << "  ";
+void imageProcessing::check2dAry(int n){
+	
+int arrayNum = n;
+if(arrayNum==1){
+	std::cout << std::endl;
+	for(int i = 0;i < this->numRows + 4;i++){
+		for(int j = 0;j < this->numCols + 4;j++){
+			if(this->frameAry[i][j] < 10){
+				std::cout << this->frameAry[i][j] << "  ";
 			} else {
-				std::cout <<array[i][j] << " ";
+				std::cout << this->frameAry[i][j] << " ";
 			}
 		}
 		std::cout << std::endl;
 	}
+} else if(arrayNum==2){
+	std::cout << std::endl;
+	for(int i = 0;i < this->numRows + 4;i++){
+		for(int j = 0;j < this->numCols + 4;j++){
+			if(this->outAry[i][j] < 10){
+				std::cout << this->outAry[i][j] << "  ";
+			} else {
+				std::cout << this->outAry[i][j] << " ";
+			}
+		}
+		std::cout << std::endl;
+		}
+	}
+	else if(arrayNum==3){
+		std::cout << std::endl; 
+		for(int i = 0;i < this->numRows + 4;i++){
+			for(int j = 0;j < this->numCols + 4;j++){
+				if(this->thrAry[i][j] < 10){
+					std::cout << this->thrAry[i][j] << "  ";
+				} else {
+					std::cout << this->thrAry[i][j] << " ";
+				}
+			}
+			std::cout << std::endl;
+		}
+	}
 }
 
 void imageProcessing::destroy1dAry(int *array){
-
+	if(array!=NULL){
+		delete [] array;
+	}	
 }
 
-void imageProcessing::destroy2dAry(int **array){
-
+void imageProcessing::destroy2dArys(){
+	if(this->frameAry!=NULL){
+		for(int i = 0;i < this->numRows + 4;i++){
+			delete this->frameAry[i];
+		}
+		delete [] this->frameAry;
+	}
+	
+	if(this->outAry!=NULL){
+		for(int i = 0;i < this->numRows + 4;i++){
+			delete this->outAry[i];
+		}
+		delete [] this->outAry;
+	}
+	
+	if(this->thrAry!=NULL){
+		for(int i = 0;i < this->numRows + 4;i++){
+			delete this->thrAry[i];
+		}
+		delete [] this->thrAry;
+	}
 }
 
 int main(int argc, char **argv) {
 	
 	std::string inFileName = argv[1];
 	std::ifstream inFile(inFileName);
+	std::ofstream outFile1(argv[2]);
+	std::ofstream outFile2(argv[3]);
 	
+	int myThrVal = std::atoi(argv[4]);
+
+	
+
 	imageProcessing myImage(inFile);
+	myImage.setThrVal(myThrVal);
 	
+// checking if arrays are allocated properly cause I'm slow like that //
+	
+	myImage.check2dAry(1);
+//	myImage.check2dAry(2);
+//	myImage.check2dAry(3);
+
+	std::cout << std::endl;
+  std::cout << myImage.getThrVal() << std::endl;	
+
+///////////////////////////////////////////////////////////////////////
+
+
+
+
+	myImage.destroy2dArys();
+	outFile1.close();
+	outFile2.close();
 
 
 
