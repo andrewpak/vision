3ff30c17c97479aec649750eb79387bd8c6f81b2
andrewpak <andrew7pak@gmail.com>
2022-03-02 20:48:22 -0500

    mirror frame done, imgReformat done

--- CornerPreserveAverage/main.cpp
+++ CornerPreserveAverage/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+#include <string>
 
 class imageProcessing {
 	
@@ -16,8 +17,14 @@ class imageProcessing {
 		imageProcessing(std::ifstream &inFile);
 		void loadImage(std::ifstream &inFile);
 		void mirrorFraming();
+		void loadMasks();
 		void setThrVal(int);
 		int getThrVal();
+		void imgReformat(int **array, int newMin, int newMax, std::ofstream &outFile);
+		int **getFrameAry();	
+		int **getOutAry();
+		int **getThrAry();
+		void threshold();
 		void setNumRows(int);
 		void setNumCols(int);
 		void setMinVal(int);
@@ -34,6 +41,7 @@ class imageProcessing {
 		void destroy1dAry(int *array);
 		void destroy2dArys();
 };
+
 
 imageProcessing::imageProcessing(std::ifstream &inFile){
 	
@@ -68,6 +76,18 @@ imageProcessing::imageProcessing(std::ifstream &inFile){
 	this->mirrorFraming();
 }
 
+int** imageProcessing::getFrameAry(){
+	return this->frameAry;
+}
+
+int** imageProcessing::getOutAry(){
+	return this->outAry;
+}
+
+int ** imageProcessing::getThrAry(){
+	return this->thrAry;
+}
+
 void imageProcessing::setThrVal(int n){
 	this->thrVal = n;
 }
@@ -100,22 +120,122 @@ void imageProcessing::mirrorFraming(){
 		if(i == 2){
 			frameAry[i - 1][numCols + 2] = frameAry[i][numCols + 1];
 			frameAry[1][numCols + 3] = frameAry[1][numCols];
-			frameAry[0][numCols + 3] = frameAry[2][numCols - 2];
+			frameAry[0][numCols + 3] = frameAry[3][numCols];
+			frameAry[0][numCols + 2] = frameAry[1][numCols + 3];
 		}
 		frameAry[i][numCols + 2] = frameAry[i][numCols + 1];
 		frameAry[i][numCols + 3] = frameAry[i][numCols];
 	}
 
 	for(int i = numCols + 2;i > 0;i--){
+		if(int i = numCols + 2){
+			frameAry[numRows + 3][numCols + 3] = frameAry[numRows][numCols];
+			frameAry[numRows + 2][numCols + 3] = frameAry[numRows + 3][numCols + 2];
+		}
 		frameAry[numRows + 2][i] = frameAry[numRows + 1][i];
 		frameAry[numRows + 3][i] = frameAry[numRows][i];
 	}
 
 	for(int i = numRows + 2;i > 0;i--){
+		if(int i = numRows + 2){
+			frameAry[i + 1][0] = frameAry[i - 2][3];
+			frameAry[i + 1][1] = frameAry[i][0];
+		}
 		frameAry[i][1] = frameAry[i][2];
 		frameAry[i][0] = frameAry[i][3]; 
 		frameAry[0][1] = frameAry[1][0];
 	}
+}
+
+
+void imageProcessing::loadMasks(){
+	std::ifstream m1("../M1.txt");
+	std::ifstream m2("../M2.txt");
+	std::ifstream m3("../M3.txt");
+	std::ifstream m4("../M4.txt");
+	std::ifstream m5("../M5.txt");
+	std::ifstream m6("../M6.txt");
+	std::ifstream m7("../M7.txt");
+	std::ifstream m8("../M8.txt");
+
+	int r, c, min, max;
+	m1 >> r >> c >> min >> max;
+	m2 >> r >> c >> min >> max;
+	m3 >> r >> c >> min >> max;
+	m4 >> r >> c >> min >> max;
+	m5 >> r >> c >> min >> max;
+	m6 >> r >> c >> min >> max;
+	m7 >> r >> c >> min >> max;
+	m8 >> r >> c >> min >> max;
+
+	std::cout << std::endl;
+	std::cout << r << " " << c << " " << min << " " << max << std::endl;
+	
+	for(int i = 0;i < 5;i++){
+		for(int j = 0;j < 5;j++){
+			m1 >> mask[0][i][j];
+			m2 >> mask[1][i][j];
+			m3 >> mask[2][i][j];
+			m4 >> mask[3][i][j];
+			m5 >> mask[4][i][j];
+			m6 >> mask[5][i][j];
+			m7 >> mask[6][i][j];
+			m8 >> mask[7][i][j];
+		}
+	}
+	for(int m = 0;m < 8;m++){
+		for(int i = 0;i < 5;i++){
+			for(int j = 0;j < 5;j++){
+				std::cout << mask[m][i][j] << " ";
+			}
+			std::cout << std::endl;
+		}
+		std::cout << std::endl;
+	}
+	
+	m1.close();
+	m2.close();
+	m3.close();
+	m4.close();
+	m5.close();
+	m6.close();
+	m7.close();
+	m8.close();
+}
+
+void imageProcessing::imgReformat(int **array, int newMin, int newMax, std::ofstream &outFile){
+	outFile << numRows << " " << numCols << " " << newMin << " " << newMax << std::endl;
+
+	std::string str = std::to_string(newMax);
+ 	std::cout << "this is str " << str << std::endl;
+
+	int Width = str.length();
+
+	std::cout << "this is Width " << Width << std::endl;
+
+	int r = 2;
+	while(r <= numRows + 1){
+	int c = 2;
+	
+	while(c <= numCols + 1){
+	outFile << array[r][c];
+
+	str = std::to_string(array[r][c]);
+	int WW = str.length();
+	while(WW <= Width){
+		outFile << " ";
+		WW++;
+	}
+	c++;
+	}
+	outFile << std::endl;
+	r++;
+	}
+}
+
+
+void imageProcessing::threshold(){
+
 
 }
 
@@ -253,9 +373,10 @@ int main(int argc, char **argv) {
 
 ///////////////////////////////////////////////////////////////////////
 
+	myImage.loadMasks();
 
-
-
+	myImage.imgReformat(myImage.getFrameAry(), myImage.getMinVal(), myImage.getMaxVal(), outFile1);
+	myImage.imgReformat(myImage.getOutAry(), 0, 0, outFile1);
 	myImage.destroy2dArys();
 	outFile1.close();
 	outFile2.close();
