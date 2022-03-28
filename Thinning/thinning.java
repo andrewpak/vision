257981e46ffb8053271e592b9622fad404545f38
andrewpak <andrew7pak@gmail.com>
2022-03-27 23:32:33 -0400

    Add Thinning

---
+++ Thinning/thinning.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+
+class thinning {
+
+
+
+    private int numRows, numCols, minVal, maxVal;
+
+    private  int changeFlag;
+
+    private int cycleCount;
+    private int [][] aryOne;
+    private int [][] aryTwo;
+
+
+    //public thinning();
+    //public void zeroFrame();
+    //public void loadImg(FileReader inFile, int [][]aryOne);
+    //public void copyArys();
+    //public void northThinning(int [][] aryOne, int [][] aryTwo);
+    //public void southThinning(int [][] aryOne, int [][] aryTwo);
+
+
+
+
+
+    public static void main(String args[]){
+       System.out.println("Hello Thinning");
+    }
+}
